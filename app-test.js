b8acc3ffd6254b3f35ff1d12b5aa84ac733630ab
Donepuchie <156867737+Donepuchie@users.noreply.github.com>
2024-09-19 21:44:56 +0100

    Update app-test.js

--- app-test.js
+++ app-test.js
@@ -11,7 +11,7 @@ chai.use(chaiHttp);
 describe('Planets API Suite', () => {
 
     describe('Fetching Planet Details', () => {
-        it('it should fetch a planet named MercurysfsffSDe', (done) => {
+        it('it should fetch a planet named MercurysfsffSD', (done) => {
             let payload = {
                 id: 1
             }
